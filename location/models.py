52db3b6d4e08f9feee5e8a5ab63862c22daa7fff
Võ Quang Nhật Hoàng <vqnhoang97tn@gmail.com>
2025-04-15 17:06:52 +0700

    Modify

--- location/models.py
+++ location/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-    id = models.CharField(max_length = 10,primary_key = True)
+    code = models.CharField(max_length=10, unique=False)
     location = models.CharField(max_length = 64)
-    city = models.CharField(max_length = 30)
+    city = models.CharField(max_length = 30)   
     type = models.CharField(max_length = 10)
+    rating = models.FloatField(default = 5)
     image_path = models.CharField(max_length=255, default='static/img/dinh-doc-lap.jpg')
     description = models.TextField(default="")
 
