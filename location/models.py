c1e765616fa12e952eaf08438480798f0b1ad11e
Võ Quang Nhật Hoàng <vqnhoang97tn@gmail.com>
2025-04-22 18:36:33 +0700

    Adding model user

--- location/models.py
+++ location/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Location(models.Model):
     code = models.CharField(max_length=10, unique=False)
     location = models.CharField(max_length = 64)
@@ -15,3 +16,12 @@ class Location(models.Model):
 
     def __str__(self):
         return f"{self.location} in {self.city}"
+    
+class User(models.Model):
+    name = models.CharField(max_length=50, default="")
+    gender = models.CharField(max_length=10, default="", blank=True)
+    email = models.EmailField(max_length=100, blank=True)
+    loclist = models.ManyToManyField(Location, blank=True, related_name="users", default = None)
+
+    def __str__(self):
+        return self.name
