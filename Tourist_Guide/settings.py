b260c0d45cf95bc4b368eb191a7b685f59ae7be9
Võ Quang Nhật Hoàng <vqnhoang97tn@gmail.com>
2025-04-25 12:20:56 +0700

    reset password

--- Tourist_Guide/settings.py
+++ Tourist_Guide/settings.py
@@ -140,5 +140,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'admin@yourdomain.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = '22520482@gm.uit.edu.vn'         # Email dùng để gửi
+EMAIL_HOST_PASSWORD = 'aini imgz fsef yhro' 
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# DEFAULT_FROM_EMAIL = 'admin@yourdomain.com'
