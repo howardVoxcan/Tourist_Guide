e47bb758816fe7d9451510d3ca4e3fd2b71b5781
Võ Quang Nhật Hoàng <vqnhoang97tn@gmail.com>
2025-04-27 10:30:49 +0700

    Using dynamic link in homepage

--- location/urls.py
+++ location/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.overall_homepage, name='overall'),
     path('my_trip/', views.my_trip, name = 'selected_locations'),
     path('favourite/',views.favourite, name = 'favourite'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('<str:location_code>',views.location_display, name='display_location'),
 ]
