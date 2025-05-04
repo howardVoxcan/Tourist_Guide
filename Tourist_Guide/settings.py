aa50642edf1423298de8fa5ca10390689b91e555
Võ Quang Nhật Hoàng <vqnhoang97tn@gmail.com>
2025-05-04 16:11:16 +0700

    secret

--- Tourist_Guide/settings.py
+++ Tourist_Guide/settings.py
@@ -148,8 +148,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = '22520482@gm.uit.edu.vn'
-EMAIL_HOST_PASSWORD = 'aini imgz fsef yhro' 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # DEFAULT_FROM_EMAIL = 'admin@yourdomain.com'
