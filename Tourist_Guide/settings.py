9174a16026be72c74622940b34854339bcd4cf71
Võ Quang Nhật Hoàng <vqnhoang97tn@gmail.com>
2025-05-05 13:14:25 +0700

    Real tour assistant

--- Tourist_Guide/settings.py
+++ Tourist_Guide/settings.py
@@ -148,6 +148,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+EMAIL_HOST_USER = 'touristguide.company@gmail.com'
+EMAIL_HOST_PASSWORD = 'qayg qrxm yywt vscp' 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # DEFAULT_FROM_EMAIL = 'admin@yourdomain.com'
