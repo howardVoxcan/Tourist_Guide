5063434e99c3a61f46a9021f4f1ad55cfa181c07
Võ Quang Nhật Hoàng <vqnhoang97tn@gmail.com>
2025-05-09 23:58:12 +0700

    tailwind

---
+++ theme/static_src/tailwind.config.js
@@ -0,0 +1,10 @@
+// static_src/tailwind.config.js
+module.exports = {
+  content: [
+    '../templates/**/*.html',  // tùy theo cấu trúc project
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
