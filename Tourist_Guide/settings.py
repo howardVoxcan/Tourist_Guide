8d6f5e17ca03c5dfd166fc4a8699c4d2f2782666
Võ Quang Nhật Hoàng <vqnhoang97tn@gmail.com>
2025-05-20 11:26:15 +0700

    sympy

--- Tourist_Guide/settings.py
+++ Tourist_Guide/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-##glyl0o&4*)7b)=v7wq&iae0liuoyt2nofq-8zt^m)og)h=se
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tourist-guide.onrender.com']
+ALLOWED_HOSTS = ['https://tourist-guide-kfel.onrender.com']
 
 CORS_ORIGIN_ALLOW_ALL = True  # or set allowed origins more securely
 
